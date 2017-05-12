c5ab69854119bde1fb5fa6a4b4cc1ee1ddfc0475
ShaneKent <sdkent@calpoly.edu>
2017-05-11 17:20:21 -0700

    Finished Part 3.

--- lab4_3.s
+++ lab4_3.s
@@ -1,15 +1,31 @@
-main:		li	$a0, 0
-		li	$a1, 1
-		li	$a2, 5
-		jal	Fibonacci
+#Lab 4, Part 3
+#Nicole Butler and Shane Kent
 
-Fibonacci:	addi	$sp, $sp, -8
-		add	$t0, $a0, $a1
-		lw	$a0, ($a1)
-		lw	$a1, ($t0)
-		addi	$t1, $a2, -1
-		sw	$t0, 0($sp)
-		sw 	$ra, 4($sp)
-		bne	$a2, $zero, ELSE
-		
-ELSE:		
+main:	li	$a0, 20
+	jal	Fibonacci
+
+end:	b 	end 
+
+Fibonacci:
+	li	$t0, 0
+	li	$t1, 1
+	li	$t2, 1
+	
+	beq	$a0, 0, done_zero
+	
+loop:	
+	beq	$t2, $a0, done
+	add	$t3, $t0, $t1
+	move	$t0, $t1
+	move	$t1, $t3
+	
+	add	$t2, $t2, 1
+	b	loop
+
+done:
+	move	$v0, $t1
+	jr	$ra
+	
+done_zero:
+	li	$v0, 0
+	jr	$ra
